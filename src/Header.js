8f0b02fcb27a89217d85c1da1888e4227881c0fd
Imroboboy1 <mpopa095@mtroyal.ca>
2021-11-09 23:50:01 -0700

    Ran Build. finished up Modal

--- src/Header.js
+++ src/Header.js
@@ -21,7 +21,8 @@ class Header extends React.Component {
                 </div>
                 <Modal isOpen={this.props.isAboutOpen}>
                     <button className="button" name="aboutClose" onClick={this.props.updateDisplay}>Close About</button>
-                    <h1>Built with React, Hosted on Heroku!</h1>
+                    
+                    <h1><img src={this.props.logo} alt="Logo" className="modalLogo"/>Built with React, Hosted on Heroku!</h1>
                     <h2>React Components Used:</h2>
                     <ul>
                         <li>react-modal: <a href="https://www.npmjs.com/package/react-modal">https://www.npmjs.com/package/react-modal</a></li>
@@ -32,6 +33,7 @@ class Header extends React.Component {
                         <li>react-highlight-words: <a href="https://www.npmjs.com/package/react-highlight-words">https://www.npmjs.com/package/react-highlight-words</a></li>
                     </ul>
                     <h2>Play Data from Randy's Server.</h2>
+                    <h3>Built By Mihai Popa</h3>
                 </Modal>
             </header>
         );
